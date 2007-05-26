4dad69bbbb8111569a3c21f1c3f74d773f2bb183
user <user@6c76846e-3230-0410-94b0-57f3116ac674>
2007-05-26 12:43:45 +0000

    t3dlib4 color table optimized
    git-svn-id: svn://127.0.0.1/T3DLIB2/trunk@11 6c76846e-3230-0410-94b0-57f3116ac674

--- T3DLIB2/src/t3dlib4.cpp
+++ T3DLIB2/src/t3dlib4.cpp
@@ -34,47 +34,51 @@ const MATRIX4X4_TYP MATRIX4X4::IDENTITY = {	1.0f, 0.0f, 0.0f, 0.0f,
 											0.0f, 0.0f, 1.0f, 0.0f,
 											0.0f, 0.0f, 0.0f, 1.0f, };
 
-T3DLIB_API unsigned char COLOR_TABLE_ADD16[MAX_COLOR_INTENSITY16][MAX_COLOR_INTENSITY16];
 T3DLIB_API unsigned char COLOR_TABLE_ADD16G[MAX_COLOR_INTENSITY16G][MAX_COLOR_INTENSITY16G];
-T3DLIB_API unsigned char COLOR_TABLE_ADD32[MAX_COLOR_INTENSITY32][MAX_COLOR_INTENSITY32];
-T3DLIB_API unsigned char COLOR_TABLE_MUL16[MAX_COLOR_INTENSITY16][MAX_COLOR_INTENSITY16];
+T3DLIB_API unsigned char COLOR_TABLE_ADD[MAX_COLOR_INTENSITY32][MAX_COLOR_INTENSITY32];
 T3DLIB_API unsigned char COLOR_TABLE_MUL16G[MAX_COLOR_INTENSITY16G][MAX_COLOR_INTENSITY16G];
-T3DLIB_API unsigned char COLOR_TABLE_MUL32[MAX_COLOR_INTENSITY32][MAX_COLOR_INTENSITY32];
+T3DLIB_API unsigned char COLOR_TABLE_MUL[MAX_COLOR_INTENSITY32][MAX_COLOR_INTENSITY32];
 
 T3DLIB_API bool Init_T3dlib4(int bpp)
 {
-	memset(COLOR_TABLE_ADD16,	0, sizeof(COLOR_TABLE_ADD16));
-	memset(COLOR_TABLE_ADD16G,	0, sizeof(COLOR_TABLE_ADD16G));
-	memset(COLOR_TABLE_ADD32,	0, sizeof(COLOR_TABLE_ADD32));
-	memset(COLOR_TABLE_MUL16,	0, sizeof(COLOR_TABLE_MUL16));
-	memset(COLOR_TABLE_MUL16G,	0, sizeof(COLOR_TABLE_MUL16G));
-	memset(COLOR_TABLE_MUL32,	0, sizeof(COLOR_TABLE_MUL32));
-
 	unsigned int i, j;
-	for(i = 0; i < MAX_COLOR_INTENSITY16; i++)
+	switch(bpp)
 	{
-		for(j = 0; j < MAX_COLOR_INTENSITY16; j++)
+	case 16:
+		for(i = 0; i < MAX_COLOR_INTENSITY16; i++)
 		{
-			COLOR_TABLE_ADD16[i][j] = (unsigned char)MIN(i + j, MAX_COLOR_INTENSITY16 - 1);
-			COLOR_TABLE_MUL16[i][j] = (unsigned char)((float)i * (float)j / (MAX_COLOR_INTENSITY16 - 1) + 0.5f);
+			for(j = 0; j < MAX_COLOR_INTENSITY16; j++)
+			{
+				COLOR_TABLE_ADD[i][j] = (unsigned char)MIN(i + j, MAX_COLOR_INTENSITY16 - 1);
+				COLOR_TABLE_MUL[i][j] = (unsigned char)((float)i * (float)j / (MAX_COLOR_INTENSITY16 - 1) + 0.5f);
+			}
 		}
-	}
-	for(i = 0; i < MAX_COLOR_INTENSITY16G; i++)
-	{
-		for(j = 0; j < MAX_COLOR_INTENSITY16G; j++)
+		for(i = 0; i < MAX_COLOR_INTENSITY16G; i++)
 		{
-			COLOR_TABLE_ADD16G[i][j] = (unsigned char)MIN(i + j, MAX_COLOR_INTENSITY16G - 1);
-			COLOR_TABLE_MUL16G[i][j] = (unsigned char)((float)i * (float)j / (MAX_COLOR_INTENSITY16G - 1) + 0.5f);
+			for(j = 0; j < MAX_COLOR_INTENSITY16G; j++)
+			{
+				COLOR_TABLE_ADD16G[i][j] = (unsigned char)MIN(i + j, MAX_COLOR_INTENSITY16G - 1);
+				COLOR_TABLE_MUL16G[i][j] = (unsigned char)((float)i * (float)j / (MAX_COLOR_INTENSITY16G - 1) + 0.5f);
+			}
 		}
-	}
-	for(i = 0; i < MAX_COLOR_INTENSITY32; i++)
-	{
-		for(j = 0; j < MAX_COLOR_INTENSITY32; j++)
+		break;
+
+	case 32:
+		for(i = 0; i < MAX_COLOR_INTENSITY32; i++)
 		{
-			COLOR_TABLE_ADD32[i][j] = (unsigned char)MIN(i + j, MAX_COLOR_INTENSITY32 - 1);
-			COLOR_TABLE_MUL32[i][j] = (unsigned char)((float)i * (float)j / (MAX_COLOR_INTENSITY32 - 1) + 0.5f);
+			for(j = 0; j < MAX_COLOR_INTENSITY32; j++)
+			{
+				COLOR_TABLE_ADD[i][j] = (unsigned char)MIN(i + j, MAX_COLOR_INTENSITY32 - 1);
+				COLOR_TABLE_MUL[i][j] = (unsigned char)((float)i * (float)j / (MAX_COLOR_INTENSITY32 - 1) + 0.5f);
+			}
 		}
+		break;
+
+	default:
+		ON_ERROR_GOTO((sprintf(gbuffer, "unsupported color bip: %d", bpp), gbuffer));
 	}
 	return true;
-	bpp;
+
+ON_ERROR:
+	return false;
 }
