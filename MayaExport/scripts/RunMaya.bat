0cbd883e9d3130ddb0d5ec2edd77ecaee97cea4e
tang <tang@bc57d0f1-8121-e340-a336-9f9962c13ea2>
2010-01-18 15:38:28 +0000

    ...
    git-svn-id: svn://127.0.0.1/repos/trunk@39 bc57d0f1-8121-e340-a336-9f9962c13ea2

--- MayaExport/scripts/RunMaya.bat
+++ MayaExport/scripts/RunMaya.bat
@@ -1,3 +1,3 @@
 set MAYA_PLUG_IN_PATH=%MAYA_PLUG_IN_PATH%;E:\Works\VC++\T3DLIB (3)\T3DSolution\Release
-set MAYA_SCRIPT_PATH=%MAYA_SCRIPT_PATH%;E:\Works\VC++\T3DLIB (3)\T3DSolution\MayaExport
+set MAYA_SCRIPT_PATH=%MAYA_SCRIPT_PATH%;E:\Works\VC++\T3DLIB (3)\T3DSolution\MayaExport\mel
 "C:\Program Files\Alias\Maya7.0\bin\maya.exe"
