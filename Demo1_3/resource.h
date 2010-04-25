61d637ea05e9f2b058ad08a889fd8c2cc80824f1
tang <tang@bc57d0f1-8121-e340-a336-9f9962c13ea2>
2010-04-25 07:31:44 +0000

    git-svn-id: svn://127.0.0.1/repos/trunk@76 bc57d0f1-8121-e340-a336-9f9962c13ea2

--- Demo1_3/resource.h
+++ Demo1_3/resource.h
@@ -9,6 +9,7 @@
 #define IDC_COMBO2                      1004
 #define IDC_COMBO3                      1005
 #define IDC_CHECK1                      1006
+#define IDC_PICTURE1                    1007
 
 // Next default values for new objects
 // 
@@ -16,7 +17,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        102
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1007
+#define _APS_NEXT_CONTROL_VALUE         1008
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
