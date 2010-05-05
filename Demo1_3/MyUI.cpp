ef760e9c959cc6d1c7f067123ebf18e4278573dd
tang <tang@bc57d0f1-8121-e340-a336-9f9962c13ea2>
2010-05-05 14:05:07 +0000

    discomment removing dialog pointer from global map
    git-svn-id: svn://127.0.0.1/repos/trunk@86 bc57d0f1-8121-e340-a336-9f9962c13ea2

--- Demo1_3/MyUI.cpp
+++ Demo1_3/MyUI.cpp
@@ -2,6 +2,10 @@
 #include "StdAfx.h"
 #include "MyUI.h"
 
+MyUIElement::MyUIElement(void)
+{
+}
+
 MyUIElement::MyUIElement(const CRect & rect)
 	: m_rect(rect)
 {
@@ -11,54 +15,61 @@ MyUIElement::~MyUIElement(void)
 {
 }
 
-MyUIRectangle::MyUIRectangle(const CRect & rect, const my::Color & color)
-	: MyUIElement(rect)
-	, m_color(color)
+MyUIProgressBar::MyUIProgressBar(void)
 {
-}
-
-MyUIRectangle::~MyUIRectangle(void)
-{
-}
-
-void MyUIRectangle::draw(t3d::RenderContext * rc)
-{
-	LONG y = m_rect.top;
-	for(; y < m_rect.bottom; y++)
-	{
-		rc->drawHorizonLine(m_rect.left, y, m_rect.Width(), m_color);
-	}
 }
 
 MyUIProgressBar::MyUIProgressBar(const CRect & rect, const my::Color & color)
-	: MyUIElement(rect)
+	: MyUIElement()
 	, m_color(color)
 	, m_percent(0)
 {
+	setRect(rect);
 }
 
 MyUIProgressBar::~MyUIProgressBar(void)
 {
 }
 
+void MyUIProgressBar::setRect(const CRect & rect)
+{
+	MyUIElement::setRect(rect);
+
+	m_nBlockCount = (m_rect.Width() - SIDE_BORDER * 2 + BLOCK_WIDTH + INTERVAL_WIDTH - 1) / (BLOCK_WIDTH + INTERVAL_WIDTH);
+
+	m_nBlockHeight = m_rect.Height() - SIDE_BORDER * 2;
+
+	m_xStart = m_rect.left + SIDE_BORDER;
+
+	m_yStart = m_rect.top + SIDE_BORDER;
+}
+
 void MyUIProgressBar::draw(t3d::RenderContext * rc)
 {
 	_ASSERT(m_percent >= 0 && m_percent <= 1);
 
-	LONG y = m_rect.top;
-	for(; y < m_rect.bottom; y++)
+	int nBlockStep = t3d::real_to_int(m_percent * m_nBlockCount + 0.5f);
+
+	for(int i = 0; i < nBlockStep; i++)
 	{
-		rc->drawHorizonLine(m_rect.left, y, t3d::real_to_int(m_rect.Width() * m_percent), m_color);
+		int x = m_xStart + i * (BLOCK_WIDTH + INTERVAL_WIDTH);
+		int width = m_rect.right - SIDE_BORDER - x;
+		rc->fillSurface(CRect(CPoint(x, m_yStart), CSize(std::min<int>(width, BLOCK_WIDTH), m_nBlockHeight)), getColor());
 	}
 }
 
-MyUIProgressBarBox::MyUIProgressBarBox(const CRect & rect)
+MyUIProgressBarBox::MyUIProgressBarBox(
+		const CRect & rect,
+		const my::Color & color /*= my::Color(0.3f, 0.3f, 0.3f)*/,
+		const my::Color & barColor /*= my::Color(0.7f, 0.7f, 0.5f)*/)
 	: MyUIElement(rect)
-	, m_rectangle(CRect(), my::Color(0.3f, 0.3f, 0.3f))
 	, m_titleRect(CRect())
-	, m_progressBar(CRect(), my::Color(0.7f, 0.7f, 0.5f))
+	, m_color(color)
+	, m_progressBar()
 {
 	setRect(rect);
+	m_progressBar.setColor(barColor);
+	m_progressBar.setPercent(0);
 }
 
 MyUIProgressBarBox::~MyUIProgressBarBox(void)
@@ -69,29 +80,23 @@ void MyUIProgressBarBox::setRect(const CRect & rect)
 {
 	MyUIElement::setRect(rect);
 
-	m_rectangle.setRect(rect);
-
 	m_titleRect.left = m_rect.left + SIDE_BORDER;
 	m_titleRect.right = m_rect.right - SIDE_BORDER;
 	m_titleRect.top = m_rect.top + SIDE_BORDER;
 	m_titleRect.bottom = m_titleRect.top + TITLE_HEIGHT;
 
-	m_progressBar.m_rect.left = m_rect.left + SIDE_BORDER;
-	m_progressBar.m_rect.right = m_rect.right - SIDE_BORDER;
-	m_progressBar.m_rect.top = m_titleRect.bottom + (m_rect.bottom - m_titleRect.bottom - BAR_HEIGHT) / 2;
-	m_progressBar.m_rect.bottom = m_progressBar.m_rect.top + BAR_HEIGHT;
-}
+	CRect progressBarRect;
+	progressBarRect.left = m_rect.left + SIDE_BORDER;
+	progressBarRect.right = m_rect.right - SIDE_BORDER;
+	progressBarRect.top = m_titleRect.bottom + (m_rect.bottom - m_titleRect.bottom - BAR_HEIGHT) / 2;
+	progressBarRect.bottom = progressBarRect.top + BAR_HEIGHT;
 
-void MyUIProgressBarBox::setPercent(t3d::real percent)
-{
-	_ASSERT(percent >= 0 && percent <= 1);
-
-	m_progressBar.m_percent = percent;
+	m_progressBar.setRect(progressBarRect);
 }
 
 void MyUIProgressBarBox::draw(t3d::RenderContext * rc)
 {
-	m_rectangle.draw(rc);
+	rc->fillSurface(getRect(), getColor());
 
 	m_progressBar.draw(rc);
 }
