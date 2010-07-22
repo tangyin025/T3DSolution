e72200afed7836ba0aca4e0eb616e730bd8b0e18
tang <tang@bc57d0f1-8121-e340-a336-9f9962c13ea2>
2010-07-22 10:47:51 +0000

    git-svn-id: svn://127.0.0.1/repos/trunk@115 bc57d0f1-8121-e340-a336-9f9962c13ea2

--- Demo1_3/MyGameEx.cpp
+++ Demo1_3/MyGameEx.cpp
@@ -351,6 +351,7 @@ const std::basic_string<charT> MyGameState::s_name(_T("MyGameState"));
 
 MyGameState::MyGameState(MyGameEx * game)
 	: m_game(game)
+	, my::World(256)
 {
 }
 
@@ -395,10 +396,16 @@ void MyGameState::leaveState(void)
 	// //////////////////////////////////////////////////////////////////////////////////////////
 }
 
+unsigned MyGameState::generateContacts(my::Contact * contacts, unsigned limits)
+{
+	return 0;
+}
+
 bool MyGameState::doFrame(void)
 {
 	// exit application by return false with user input 'escape'
-	if(m_game->m_keyboard->isKeyDown(DIK_ESCAPE))
+	t3d::DIKeyboard * keyboard = m_game->m_keyboard.get();
+	if(keyboard->isKeyDown(DIK_ESCAPE))
 	{
 		return false;
 	}
@@ -420,34 +427,35 @@ bool MyGameState::doFrame(void)
 	//m_rc->setClipperRect(clipper);
 
 	// clear back surface with gray color
-	m_game->m_rc->fillSurface(m_game->m_rc->getClipperRect(), my::Color(0.8f, 0.8f, 0.8f));
+	t3d::RenderContext * rc = m_game->m_rc.get();
+	rc->fillSurface(rc->getClipperRect(), my::Color(0.8f, 0.8f, 0.8f));
 
 	// clear zbuffer with infinite distance
-	m_game->m_rc->fillZbuffer(m_game->m_rc->getClipperRect(), 0);
+	rc->fillZbuffer(rc->getClipperRect(), 0);
 
 	// set render context camera
-	m_game->m_rc->setViewport(m_game->m_rc->getClipperRect());
-	m_game->m_rc->setCameraProjection(t3d::CameraContext::buildCameraProjectionFOVAuto(DEG_TO_RAD(90), m_game->m_rc->getClipperRect().Width(), m_game->m_rc->getClipperRect().Height()));
-	m_game->m_rc->setCameraNearZ(1);
-	m_game->m_rc->setCameraFarZ(10000);
+	rc->setViewport(rc->getClipperRect());
+	rc->setCameraProjection(t3d::CameraContext::buildCameraProjectionFOVAuto(DEG_TO_RAD(90), rc->getClipperRect().Width(), rc->getClipperRect().Height()));
+	rc->setCameraNearZ(1);
+	rc->setCameraFarZ(10000);
 
 	// update euler cameras position and orientation by user input
 	m_eulerCam->update(m_game->m_keyboard.get(), m_game->m_mouse.get(), elapsedTime);
-	m_game->m_rc->setCameraMatrix(t3d::CameraContext::buildInverseCameraTransformEuler(m_eulerCam->getPosition(), m_eulerCam->getRotation()));
+	rc->setCameraMatrix(t3d::CameraContext::buildInverseCameraTransformEuler(m_eulerCam->getPosition(), m_eulerCam->getRotation()));
 
 	// set render context lights
 	my::Vec4<real> l_pos(-30, 30, -30);
 	l_pos *= t3d::mat3RotZXY(m_eulerCam->getRotation()) * t3d::mat3Mov(m_eulerCam->getPosition());
-	m_game->m_rc->clearLightList();
-	m_game->m_rc->pushLightAmbient(my::Vec4<real>(0.2f, 0.2f, 0.2f));
-	m_game->m_rc->pushLightPoint(my::Vec4<real>(1, 1, 1), l_pos); //my::Vec4<real>(100, 100, -100));
+	rc->clearLightList();
+	rc->pushLightAmbient(my::Vec4<real>(0.2f, 0.2f, 0.2f));
+	rc->pushLightPoint(my::Vec4<real>(1, 1, 1), l_pos); //my::Vec4<real>(100, 100, -100));
 
 	// set render context material
-	m_game->m_rc->setAmbient(my::Color::WHITE);
-	m_game->m_rc->setDiffuse(my::Color::WHITE);
+	rc->setAmbient(my::Color::WHITE);
+	rc->setDiffuse(my::Color::WHITE);
 
 	// draw default grid, with use to test distance of the scene
-	m_grid->drawZBufferRW(m_game->m_rc.get());
+	m_grid->drawZBufferRW(rc);
 
 	// //////////////////////////////////////////////////////////////////////////////////////////
 
@@ -469,7 +477,7 @@ bool MyGameState::doFrame(void)
 	ds3dl.vPosition.x = m_eulerCam->getPosition().x;
 	ds3dl.vPosition.y = m_eulerCam->getPosition().y;
 	ds3dl.vPosition.z = m_eulerCam->getPosition().z;
-	my::Mat4<real> camMatRot = t3d::mat4GetRotationScalePart(m_game->m_rc->getCameraMatrix().inverse());
+	my::Mat4<real> camMatRot = t3d::mat4GetRotationScalePart(rc->getCameraMatrix().inverse());
 	my::Vec4<real> vfront = my::Vec4<real>::UNIT_Z.transform(camMatRot);
 	ds3dl.vOrientFront.x = vfront.x;
 	ds3dl.vOrientFront.y = vfront.y;
@@ -485,43 +493,43 @@ bool MyGameState::doFrame(void)
 
 	real idxZ = -50, lstZ = -50;
 
-	m_game->m_rc->setTextureBuffer(m_objImg->getBits(), m_objImg->getPitch(), m_objImg->getWidth(), m_objImg->getHeight());
+	rc->setTextureBuffer(m_objImg->getBits(), m_objImg->getPitch(), m_objImg->getWidth(), m_objImg->getHeight());
 
-	m_obj->drawWireZBufferRW(m_game->m_rc.get(), my::Color::BLUE, t3d::mat3Mov(my::Vec4<real>(10, 0, idxZ += 10)));
+	m_obj->drawWireZBufferRW(rc, my::Color::BLUE, t3d::mat3Mov(my::Vec4<real>(10, 0, idxZ += 10)));
 
-	m_lstObj->drawWireZBufferRW(m_game->m_rc.get(), my::Color::BLUE, t3d::mat3Mov(my::Vec4<real>(-10, 0, lstZ += 10)));
+	m_lstObj->drawWireZBufferRW(rc, my::Color::BLUE, t3d::mat3Mov(my::Vec4<real>(-10, 0, lstZ += 10)));
 
-	m_obj->drawZBufferRW(m_game->m_rc.get(), my::Color::BLUE, t3d::mat3Mov(my::Vec4<real>(10, 0, idxZ += 10)));
+	m_obj->drawZBufferRW(rc, my::Color::BLUE, t3d::mat3Mov(my::Vec4<real>(10, 0, idxZ += 10)));
 
-	m_lstObj->drawZBufferRW(m_game->m_rc.get(), my::Color::BLUE, t3d::mat3Mov(my::Vec4<real>(-10, 0, lstZ += 10)));
+	m_lstObj->drawZBufferRW(rc, my::Color::BLUE, t3d::mat3Mov(my::Vec4<real>(-10, 0, lstZ += 10)));
 
-	m_obj->drawGouraudZBufferRW(m_game->m_rc.get(), t3d::mat3Mov(my::Vec4<real>(10, 0, idxZ += 10)), my::Mat4<real>::IDENTITY);
+	m_obj->drawGouraudZBufferRW(rc, t3d::mat3Mov(my::Vec4<real>(10, 0, idxZ += 10)), my::Mat4<real>::IDENTITY);
 
-	m_lstObj->drawGouraudZBufferRW(m_game->m_rc.get(), t3d::mat3Mov(my::Vec4<real>(-10, 0, lstZ += 10)), my::Mat4<real>::IDENTITY);
+	m_lstObj->drawGouraudZBufferRW(rc, t3d::mat3Mov(my::Vec4<real>(-10, 0, lstZ += 10)), my::Mat4<real>::IDENTITY);
 
-	m_obj->drawTextureZBufferW(m_game->m_rc.get(), t3d::mat3Mov(my::Vec4<real>(10, 0, idxZ += 10)));
+	m_obj->drawTextureZBufferW(rc, t3d::mat3Mov(my::Vec4<real>(10, 0, idxZ += 10)));
 
-	m_lstObj->drawTextureZBufferW(m_game->m_rc.get(), t3d::mat3Mov(my::Vec4<real>(-10, 0, lstZ += 10)));
+	m_lstObj->drawTextureZBufferW(rc, t3d::mat3Mov(my::Vec4<real>(-10, 0, lstZ += 10)));
 
-	m_obj->drawTexturePerspectiveLPZBufferW(m_game->m_rc.get(), t3d::mat3Mov(my::Vec4<real>(10, 0, idxZ += 10)));
+	m_obj->drawTexturePerspectiveLPZBufferW(rc, t3d::mat3Mov(my::Vec4<real>(10, 0, idxZ += 10)));
 
-	m_lstObj->drawTexturePerspectiveLPZBufferW(m_game->m_rc.get(), t3d::mat3Mov(my::Vec4<real>(-10, 0, lstZ += 10)));
+	m_lstObj->drawTexturePerspectiveLPZBufferW(rc, t3d::mat3Mov(my::Vec4<real>(-10, 0, lstZ += 10)));
 
-	m_obj->drawTextureZBufferRW(m_game->m_rc.get(), t3d::mat3Mov(my::Vec4<real>(10, 0, idxZ += 10)));
+	m_obj->drawTextureZBufferRW(rc, t3d::mat3Mov(my::Vec4<real>(10, 0, idxZ += 10)));
 
-	m_lstObj->drawTextureZBufferRW(m_game->m_rc.get(), t3d::mat3Mov(my::Vec4<real>(-10, 0, lstZ += 10)));
+	m_lstObj->drawTextureZBufferRW(rc, t3d::mat3Mov(my::Vec4<real>(-10, 0, lstZ += 10)));
 
-	m_obj->drawTexturePerspectiveLPZBufferRW(m_game->m_rc.get(), t3d::mat3Mov(my::Vec4<real>(10, 0, idxZ += 10)));
+	m_obj->drawTexturePerspectiveLPZBufferRW(rc, t3d::mat3Mov(my::Vec4<real>(10, 0, idxZ += 10)));
 
-	m_lstObj->drawTexturePerspectiveLPZBufferRW(m_game->m_rc.get(), t3d::mat3Mov(my::Vec4<real>(-10, 0, lstZ += 10)));
+	m_lstObj->drawTexturePerspectiveLPZBufferRW(rc, t3d::mat3Mov(my::Vec4<real>(-10, 0, lstZ += 10)));
 
-	m_obj->drawGouraudTextureZBufferRW(m_game->m_rc.get(), t3d::mat3Mov(my::Vec4<real>(10, 0, idxZ += 10)), my::Mat4<real>::IDENTITY);
+	m_obj->drawGouraudTextureZBufferRW(rc, t3d::mat3Mov(my::Vec4<real>(10, 0, idxZ += 10)), my::Mat4<real>::IDENTITY);
 
-	m_lstObj->drawGouraudTextureZBufferRW(m_game->m_rc.get(), t3d::mat3Mov(my::Vec4<real>(-10, 0, lstZ += 10)), my::Mat4<real>::IDENTITY);
+	m_lstObj->drawGouraudTextureZBufferRW(rc, t3d::mat3Mov(my::Vec4<real>(-10, 0, lstZ += 10)), my::Mat4<real>::IDENTITY);
 
-	m_obj->drawGouraudTexturePerspectiveLPZBufferRW(m_game->m_rc.get(), t3d::mat3Mov(my::Vec4<real>(10, 0, idxZ += 10)), my::Mat4<real>::IDENTITY);
+	m_obj->drawGouraudTexturePerspectiveLPZBufferRW(rc, t3d::mat3Mov(my::Vec4<real>(10, 0, idxZ += 10)), my::Mat4<real>::IDENTITY);
 
-	m_lstObj->drawGouraudTexturePerspectiveLPZBufferRW(m_game->m_rc.get(), t3d::mat3Mov(my::Vec4<real>(-10, 0, lstZ += 10)), my::Mat4<real>::IDENTITY);
+	m_lstObj->drawGouraudTexturePerspectiveLPZBufferRW(rc, t3d::mat3Mov(my::Vec4<real>(-10, 0, lstZ += 10)), my::Mat4<real>::IDENTITY);
 
 	// //////////////////////////////////////////////////////////////////////////////////////////
 
@@ -529,8 +537,8 @@ bool MyGameState::doFrame(void)
 	std::basic_string<charT> strTmp;
 	HDC hdc = m_game->m_backSurface->getDC();
 
-	int textx = m_game->m_rc->getClipperRect().left + 10;
-	int texty = m_game->m_rc->getClipperRect().top + 10;
+	int textx = rc->getClipperRect().left + 10;
+	int texty = rc->getClipperRect().top + 10;
 
 	strTmp = str_printf(_T("fps: %.1f"), m_fpsMgr->getFPS());
 	::TextOut(hdc, textx, texty, strTmp.c_str(), (int)strTmp.length());
@@ -538,7 +546,7 @@ bool MyGameState::doFrame(void)
 	strTmp = str_printf(_T("fps: %.1f"), 1 / elapsedTime);
 	::TextOut(hdc, textx, texty += 20, strTmp.c_str(), (int)strTmp.length());
 
-	t3d::Vec4<real> camPos = m_game->m_rc->getCameraPosition();
+	t3d::Vec4<real> camPos = rc->getCameraPosition();
 	strTmp = str_printf(_T("cam.pos: %f, %f, %f"),
 		camPos.x, camPos.y, camPos.z);
 	::TextOut(hdc, textx, texty += 20, strTmp.c_str(), (int)strTmp.length());
