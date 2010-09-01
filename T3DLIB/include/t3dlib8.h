69550192957a01e3f10380c3d551dfed650989db
tang <tang@bc57d0f1-8121-e340-a336-9f9962c13ea2>
2010-09-01 15:28:48 +0000

    add t3d::SilhouetteEdge
    git-svn-id: svn://127.0.0.1/repos/trunk@126 bc57d0f1-8121-e340-a336-9f9962c13ea2

--- T3DLIB/include/t3dlib8.h
+++ T3DLIB/include/t3dlib8.h
@@ -97,6 +97,64 @@ namespace t3d
 		const VertexIndexList & vertexIndexList,
 		ClipStateList & clipStateList,
 		const Vec4<real> & eye);
+
+	class SilhouetteEdge
+	{
+	public:
+		Vec4<real> v0;
+		Vec4<real> v1;
+		size_t tri0_i;
+		size_t tri1_i;
+	};
+
+	typedef std::vector<SilhouetteEdge> SilhouetteEdgeList;
+
+	SilhouetteEdgeList & buildSilhouetteEdgeListFromTriangleList(
+		SilhouetteEdgeList & retSilhouetteEdgeList,
+		const VertexList & vertexList);
+
+	SilhouetteEdgeList & buildSilhouetteEdgeListFromTriangleIndexList(
+		SilhouetteEdgeList & retSilhouetteEdgeList,
+		const VertexList & vertexList,
+		const VertexIndexList & vertexIndexList);
+
+	typedef std::vector<real> IndicatorList;
+
+	IndicatorList & buildIndicatorListFromTriangleListByPoint(
+		IndicatorList & retIndicatorList,
+		const VertexList & vertexList,
+		const Vec4<real> & point);
+
+	IndicatorList & buildIndicatorListFromTriangleIndexListByPoint(
+		IndicatorList & retIndicatorList,
+		const VertexList & vertexList,
+		const VertexIndexList & vertexIndexList,
+		const Vec4<real> & point);
+
+	IndicatorList & buildIndicatorListFromTriangleListByDirection(
+		IndicatorList & retIndicatorList,
+		const VertexList & vertexList,
+		const Vec4<real> & direction);
+
+	IndicatorList & buildIndicatorListFromTriangleIndexListByDirection(
+		IndicatorList & retIndicatorList,
+		const VertexList & vertexList,
+		const VertexIndexList & vertexIndexList,
+		const Vec4<real> & direction);
+
+	VertexList & buildShadowVolumeByPoint(
+		VertexList & retVertexList,
+		const SilhouetteEdgeList & silhouetteEdgeList,
+		const IndicatorList & indicatorList,
+		const Vec4<real> & point,
+		real distance);
+
+	VertexList & buildShadowVolumeByDirection(
+		VertexList & retVertexList,
+		const SilhouetteEdgeList & silhouetteEdgeList,
+		const IndicatorList & indicatorList,
+		const Vec4<real> & direction,
+		real distance);
 }
 
 #endif // __T3DLIB8_H__
