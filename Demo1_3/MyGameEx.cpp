e13b6780603c86fbde46fff3a06a5b2af83e8c3c
tang <tang@bc57d0f1-8121-e340-a336-9f9962c13ea2>
2011-04-14 13:03:24 +0000

    implemented original demo1_2 (skeleton animation & shadow volume) in demo1_3
    git-svn-id: svn://127.0.0.1/repos/trunk@181 bc57d0f1-8121-e340-a336-9f9962c13ea2

--- Demo1_3/MyGameEx.cpp
+++ Demo1_3/MyGameEx.cpp
@@ -4,18 +4,22 @@
 
 LRESULT MyWindow::OnClose(UINT nMsg, WPARAM wParam, LPARAM lParam, BOOL & bHandled)
 {
-	// for load state, there must be waiting for another thread to prepare for quiting
-	if(MyGame::getSingleton().getCurrentStateName() == MyLoadState::s_name)
+	// for game state, exit application directly
+	if(MyGame::getSingleton().getCurrentStateName() == MyGameState::s_name)
 	{
-		MyLoadStatePtr loadState = MyGame::getSingleton().getCurrentState<MyLoadState>();
-		if(!loadState->getExitFlagSync())
-		{
-			loadState->setExitFlagSync();
-			return 0;
-		}
+		bHandled = FALSE;
+		return 0;
 	}
 
-	bHandled = FALSE;
+	// else there must be waiting for another thread to prepare for quiting
+	_ASSERT(MyGame::getSingleton().getCurrentStateName() == MyLoadState::s_name);
+	MyLoadStatePtr loadState = MyGame::getSingleton().getCurrentState<MyLoadState>();
+	if(!loadState->getExitFlagSync())
+	{
+		loadState->setExitFlagSync();
+	}
+
+	_ASSERT(TRUE == bHandled);
 	return 0;
 }
 
@@ -291,29 +295,148 @@ bool MyLoadState::doFrame(real elapsedTime)
 	return true;
 }
 
+#define EXIT_OR_UPDATE_PROGRESS(percent) \
+	if(getExitFlagSync()) \
+	{ \
+		return 0; \
+	} \
+	updateProgressSync(percent);
+
 DWORD MyLoadState::onProc(void)
 {
 	// NOTE: because of multi-thread frame, the try block in the main thread
 	// could not catch any exceptions which was thrown from the this thread proc
 	try
 	{
-		m_progressBoxLock.enter();
-		m_progressBox->setTitleText(_T("読み取っています ..."));
-		m_progressBoxLock.leave();
+		updateTitleSync(_T("Loading ..."));
 
 		MyGameState * gameState = MyGame::getSingleton().getState<MyGameState>(MyGameState::s_name).get();
 		_ASSERT(NULL != gameState);
 
 		// HERE, INITIAL gameState objects that are time-consuming
+		const int total_step = 14;
+		int current_step = 1;
 
-		const int loopCount = 5;
-		for(int i = 1; i <= loopCount; i++)
+		// exit or update progress percent
+		EXIT_OR_UPDATE_PROGRESS((real)current_step++ / total_step);
+
+		// load scene
+		my::ObjectFromOgreMesh scene_obj(
+			my::IOStreamPtr(new my::FileStream(my::ResourceMgr::getSingleton().findFileOrException(_T("office_tri_list.mesh.xml")))));
+
+		EXIT_OR_UPDATE_PROGRESS((real)current_step++ / total_step);
+
+		// load scene texture
+		my::ImagePtr scene_tex = my::ColorConversion::getSingleton().convertImage(
+			my::ImagePtr(new my::Image(my::ResourceMgr::getSingleton().findFileOrException(_T("office_texture.jpg")))));
+
+		EXIT_OR_UPDATE_PROGRESS((real)current_step++ / total_step);
+
+		// build bsp scene
+		gameState->m_scene_bsp = my::buildFrontToBackBspSceneGouraudTexturePerspectiveLPZBufferRW(
+			scene_obj.getVertexList(),
+			scene_obj.getNormalList(),
+			scene_obj.getUVList(),
+			my::MaterialPtr(new my::Material(my::Color::WHITE, my::Color::WHITE)),
+			scene_tex);
+
+		EXIT_OR_UPDATE_PROGRESS((real)current_step++ / total_step);
+
+		// load jack bone assignment model
+		my::BoneAssignmentIndexObjectFromOgreMeshPtr bone_assignment_obj;
+		bone_assignment_obj = my::BoneAssignmentIndexObjectFromOgreMeshPtr(new my::BoneAssignmentIndexObjectFromOgreMesh(
+			my::IOStreamPtr(new my::FileStream(my::ResourceMgr::getSingleton().findFileOrException(_T("jack_hres.mesh.xml"))))));
+		gameState->m_jack_bone_assignments.push_back(bone_assignment_obj);
+
+		EXIT_OR_UPDATE_PROGRESS((real)current_step++ / total_step);
+
+		// load jack texture
+		my::ImagePtr jack_tex = my::ColorConversion::getSingleton().convertImage(
+			my::ImagePtr(new my::Image(my::ResourceMgr::getSingleton().findFileOrException(_T("jack_texture.jpg")))));
+
+		EXIT_OR_UPDATE_PROGRESS((real)current_step++ / total_step);
+
+		// copy original mesh from jack bone assignment to custom shader obj
+		my::CustomShaderIndexObjectPtr custom_shader_obj;
+		custom_shader_obj = my::CustomShaderIndexObjectPtr(new my::CustomShaderIndexObjectGouraudTextureZBufferRW(
+			my::MaterialPtr(new my::Material(my::Color::WHITE, my::Color::WHITE)), jack_tex));
+		custom_shader_obj->getVertexList() = bone_assignment_obj->getOriginalVertexList();
+		custom_shader_obj->getVertexIndexList() = bone_assignment_obj->getVertexIndexList();
+		custom_shader_obj->getNormalList() = bone_assignment_obj->getOriginalNormalList();
+		custom_shader_obj->getUVList() = bone_assignment_obj->getUVList();
+		gameState->m_jack_meshs.push_back(custom_shader_obj);
+
+		EXIT_OR_UPDATE_PROGRESS((real)current_step++ / total_step);
+
+		// build connection edge
+		gameState->m_jack_connection_edges.push_back(t3d::ConnectionEdgeList());
+		MyGameState::ConnectionEdgeListList::reverse_iterator connect_edge_iter;
+		connect_edge_iter = gameState->m_jack_connection_edges.rbegin();
+		t3d::buildConnectionEdgeListFromTriangleIndexList(
+			*connect_edge_iter,
+			custom_shader_obj->getVertexList(),
+			custom_shader_obj->getVertexIndexList());
+
+		EXIT_OR_UPDATE_PROGRESS((real)current_step++ / total_step);
+
+		// load jack hair as the same
+		bone_assignment_obj = my::BoneAssignmentIndexObjectFromOgreMeshPtr(new my::BoneAssignmentIndexObjectFromOgreMesh(
+			my::IOStreamPtr(new my::FileStream(my::ResourceMgr::getSingleton().findFileOrException(_T("jack_hres_hair.mesh.xml"))))));
+		gameState->m_jack_bone_assignments.push_back(bone_assignment_obj);
+
+		EXIT_OR_UPDATE_PROGRESS((real)current_step++ / total_step);
+
+		custom_shader_obj = my::CustomShaderIndexObjectPtr(new my::CustomShaderIndexObjectGouraudTextureZBufferRWWithBackface(
+			my::MaterialPtr(new my::Material(my::Color::WHITE, my::Color::WHITE)), jack_tex));
+		custom_shader_obj->getVertexList() = bone_assignment_obj->getOriginalVertexList();
+		custom_shader_obj->getVertexIndexList() = bone_assignment_obj->getVertexIndexList();
+		custom_shader_obj->getNormalList() = bone_assignment_obj->getOriginalNormalList();
+		custom_shader_obj->getUVList() = bone_assignment_obj->getUVList();
+		gameState->m_jack_meshs.push_back(custom_shader_obj);
+
+		EXIT_OR_UPDATE_PROGRESS((real)current_step++ / total_step);
+
+		gameState->m_jack_connection_edges.push_back(t3d::ConnectionEdgeList());
+		connect_edge_iter = gameState->m_jack_connection_edges.rbegin();
+		t3d::buildConnectionEdgeListFromTriangleIndexList(
+			*connect_edge_iter,
+			custom_shader_obj->getVertexList(),
+			custom_shader_obj->getVertexIndexList());
+
+		EXIT_OR_UPDATE_PROGRESS((real)current_step++ / total_step);
+
+		// load jack skeleton animation
+		my::SkeletonAnimationsFromOgreSkeletonPtr skeleton_anim(new my::SkeletonAnimationsFromOgreSkeleton(
+			my::IOStreamPtr(new my::FileStream(my::ResourceMgr::getSingleton().findFileOrException(_T("jack_anim_stand.skeleton.xml"))))));
+		skeleton_anim->getSkeletonAnimationNode("clip1").setNextAnimationName("clip2");
+		skeleton_anim->getSkeletonAnimationNode("clip2").setNextAnimationName("clip1");
+		skeleton_anim->getSkeletonAnimationNode("clip3").setNextAnimationName("clip4");
+		skeleton_anim->getSkeletonAnimationNode("clip4").setNextAnimationName("clip3");
+		skeleton_anim->setCurrentAnimationName("clip1");
+		skeleton_anim->setCurrentAnimationTime(0);
+		gameState->m_jack_skel = skeleton_anim;
+
+		EXIT_OR_UPDATE_PROGRESS((real)current_step++ / total_step);
+
+		// load sky sphere
+		gameState->m_sky_sphere = my::IndexSphereObjectPtr(new my::IndexSphereObject(50000.0f, 20, 20, true));
+		gameState->m_sky_sphere_tex = my::ColorConversion::getSingleton().convertImage(
+			my::ImagePtr(new my::Image(my::ResourceMgr::getSingleton().findFileOrException(_T("1316532925.jpg")))));
+
+		EXIT_OR_UPDATE_PROGRESS((real)current_step++ / total_step);
+
+		// load & play mp3
+		std::basic_string<t3d::charT> mp3_path = my::ResourceMgr::getSingleton().findFile(_T("i am the wind.mp3"));
+		if(!mp3_path.empty())
 		{
-			::Sleep(100);
-			m_progressBoxLock.enter();
-			m_progressBox->setPercent((real)i / loopCount);
-			m_progressBoxLock.leave();
+			gameState->m_music = my::Mp3Ptr(new my::Mp3(MyGame::getSingleton().m_dsound, my::IOStreamPtr(new my::FileStream(mp3_path))));
+			gameState->m_music->play(true);
 		}
+
+		EXIT_OR_UPDATE_PROGRESS((real)current_step++ / total_step);
+
+		// sleep a moment
+		Sleep(300);
 	}
 	catch(t3d::Exception & e)
 	{
@@ -371,23 +494,38 @@ void MyGameState::enterState(void)
 	// initial camera hander particle
 	m_hander.particle = my::ParticlePtr(new my::Particle());
 	m_hander.particle->setMass(1);
-	m_hander.particle->setPosition(t3d::vec3Add(m_character.body->getPosition(), my::Vec4<real>(0, 10, 0)));
+	m_hander.particle->setPosition(t3d::vec3Add(m_character.body->getPosition(), my::Vec4<real>(0, 20, 0)));
 	m_hander.particle->setVelocity(my::Vec4<real>::ZERO);
 	m_hander.particle->setDamping(0.00001f); // ***
 	m_hander.particle->setAcceleration(my::Vec4<real>::ZERO);
 	particleList.push_back(m_hander.particle);
 
 	// initial camera hander spring
-	m_hander.spring = my::ParticleAnchoredSpringPtr(new my::ParticleAnchoredSpring(m_character.body->getPosition(), 100, 10)); // ***
+	m_hander.spring = my::ParticleAnchoredSpringPtr(new my::ParticleAnchoredSpring(m_character.body->getPosition(), 200, 20)); // ***
 	ParticleWorld::registry.add(m_hander.particle.get(), m_hander.spring.get());
 
 	// initial camera hander cable
-	m_hander.cable = my::ParticleCableConstraintPtr(new my::ParticleCableConstraint(m_character.body->getPosition(), m_hander.particle.get(), 13, 0.0f));
+	m_hander.cable = my::ParticleCableConstraintPtr(new my::ParticleCableConstraint(m_character.body->getPosition(), m_hander.particle.get(), 23, 0.0f));
 	particleContactGeneratorList.push_back(m_hander.cable);
 
 	// initial global physics ground
 	m_ground.setNormal(my::Vec4<real>::UNIT_Y);
 	m_ground.setDistance(0);
+
+	// check time-consuming resources are created
+	_ASSERT(NULL != m_scene_bsp);
+	_ASSERT(2 == m_jack_bone_assignments.size());
+	_ASSERT(m_jack_bone_assignments.size() == m_jack_meshs.size());
+	_ASSERT(m_jack_bone_assignments.size() == m_jack_connection_edges.size());
+	_ASSERT(NULL != m_jack_skel);
+	_ASSERT(NULL != m_sky_sphere);
+	_ASSERT(NULL != m_sky_sphere_tex);
+
+	// initialize some temp structs
+	m_jack_intersection_offset = 0.5f;
+	m_jack_combine_bone_node_list = m_jack_skel->getBoneNodeList();
+	m_jack_bone_node_list = m_jack_skel->getBoneNodeList();
+	m_jack_bone_transform_list.resize(m_jack_skel->getBoneNodeListSize());
 }
 
 void MyGameState::leaveState(void)
@@ -417,6 +555,8 @@ void MyGameState::integrate(real duration)
 			t3d::Vec4<real> axis = my::Vec4<real>::UNIT_Y;
 			m_character.body->setOrientation(m_character.body->getOrientation() * t3d::buildQuatFromAngleAxis(angle, axis));
 		}
+
+		// set awake
 		m_character.body->setAwake(true);
 	}
 
@@ -488,11 +628,11 @@ bool MyGameState::doFrame(real elapsedTime)
 		return false;
 	}
 
-	// clear back surface with gray color
-	rc->fillSurface(rc->getClipperRect(), my::Color(0.8f, 0.8f, 0.8f));
+	//// clear back surface with gray color
+	//rc->fillSurface(rc->getClipperRect(), my::Color(0.8f, 0.8f, 0.8f));
 
-	// clear zbuffer with infinite distance
-	rc->fillZbuffer(rc->getClipperRect(), 0);
+	//// clear zbuffer with infinite distance
+	//rc->fillZbuffer(rc->getClipperRect(), 0);
 
 	// clear stencil buffer with zero
 	rc->fillStencilBuffer(rc->getClipperRect(), 0);
@@ -501,7 +641,7 @@ bool MyGameState::doFrame(real elapsedTime)
 	rc->setViewport(rc->getClipperRect());
 	rc->setCameraProjection(t3d::CameraContext::buildCameraProjectionFOVAuto(DEG_TO_RAD(90), rc->getClipperRect().Width(), rc->getClipperRect().Height()));
 	rc->setCameraNearZ(1);
-	rc->setCameraFarZ(10000);
+	rc->setCameraFarZ(100000);
 
 	if(keyboard->isKeyDown(DIK_LCONTROL))
 	{
@@ -526,57 +666,181 @@ bool MyGameState::doFrame(real elapsedTime)
 
 		// update camera matrix according to the camera hander followed with character
 		t3d::Mat4<real> matRotation = mat3RotXYZ(eulerCam->getRotation());
-		t3d::Mat4<real> matPosition = t3d::mat3Mov(my::Vec4<real>(0, 0, -30)) * matRotation * t3d::mat3Mov(m_hander.particle->getPosition());
+		t3d::Mat4<real> matPosition = t3d::mat3Mov(my::Vec4<real>(0, 0, -20)) * matRotation * t3d::mat3Mov(m_hander.particle->getPosition());
 		eulerCam->setPosition(my::Vec4<real>::ZERO * matPosition);
 	}
 
 	// set camera matrix (inversed matrix)
 	rc->setCameraMatrix(t3d::CameraContext::buildInverseCameraTransformEuler(eulerCam->getPosition(), eulerCam->getRotation()));
 
+	// draw sky sphere to write both surface and zbuffer
+	rc->setTextureBuffer(
+		m_sky_sphere_tex->getBits(),
+		m_sky_sphere_tex->getPitch(),
+		m_sky_sphere_tex->getWidth(),
+		m_sky_sphere_tex->getHeight());
+	m_sky_sphere->drawTextureZBufferWWithBackface(rc);
+
 	// set render context lights
-	my::Vec4<real> l_pos(-30, 30, -30);
-	l_pos *= t3d::mat3RotZXY(eulerCam->getRotation()) * t3d::mat3Mov(eulerCam->getPosition());
+	my::Vec4<real> l_pos(-30, 30, -10);
+	l_pos *= t3d::CameraContext::buildCameraTransformEuler(eulerCam->getPosition(), eulerCam->getRotation());
 	rc->clearLightList();
 	rc->pushLightAmbient(my::Vec4<real>(0.2f, 0.2f, 0.2f));
 	rc->pushLightPoint(my::Color::WHITE, l_pos);
 
-	// render character sphere
-	drawSphereWireZBufferRW(
-		rc,
-		m_character.sphere.getRadius(),
-		m_character.body->getAwake() ? my::Color::RED : t3d::vec3Mul(my::Color::RED, 0.7f),
-		m_character.body->getTransform());
+	//// render character sphere
+	//drawSphereWireZBufferRW(
+	//	rc,
+	//	m_character.sphere.getRadius(),
+	//	m_character.body->getAwake() ? my::Color::RED : t3d::vec3Mul(my::Color::RED, 0.7f),
+	//	m_character.body->getTransform());
 
-	// render camera hander particle
-	drawSphereWireZBufferRW(
-		rc, 1, my::Color::YELLOW, t3d::mat3Mov(m_hander.particle->getPosition()));
+	//// render camera hander particle
+	//drawSphereWireZBufferRW(
+	//	rc, 1, my::Color::YELLOW, t3d::mat3Mov(m_hander.particle->getPosition()));
 
-	// render the direction of character body
-	drawLinePointAndNormalZBufferRW(
-		rc,
-		m_character.body->getPosition(),
-		my::Vec4<real>(0, 0, 10) * m_character.body->getRotationTransform(),
-		my::Color::BLUE);
+	//// render the direction of character body
+	//drawLinePointAndNormalZBufferRW(
+	//	rc,
+	//	m_character.body->getPosition(),
+	//	my::Vec4<real>(0, 0, 10) * m_character.body->getRotationTransform(),
+	//	my::Color::BLUE);
 
-	// draw grids that always around character
-	static int gridStep = 10;
-	static int gridRange = 10;
-	int gridCentX = (int)floor(m_character.body->getPosition().x / gridStep);
-	int gridCentZ = (int)floor(m_character.body->getPosition().z / gridStep);
-	real gridStartX = (gridCentX - gridRange) * gridStep;
-	real gridStartZ = (gridCentZ - gridRange) * gridStep;
-	real gridEndX = (gridCentX + gridRange) * gridStep;
-	real gridEndZ = (gridCentZ + gridRange) * gridStep;
-	rc->clearVertexList();
-	for(int i = -gridRange; i <= gridRange; i++)
+	//// draw grids that always around character
+	//static int gridStep = 10;
+	//static int gridRange = 10;
+	//int gridCentX = (int)floor(m_character.body->getPosition().x / gridStep);
+	//int gridCentZ = (int)floor(m_character.body->getPosition().z / gridStep);
+	//real gridStartX = (gridCentX - gridRange) * gridStep;
+	//real gridStartZ = (gridCentZ - gridRange) * gridStep;
+	//real gridEndX = (gridCentX + gridRange) * gridStep;
+	//real gridEndZ = (gridCentZ + gridRange) * gridStep;
+	//rc->clearVertexList();
+	//for(int i = -gridRange; i <= gridRange; i++)
+	//{
+	//	rc->pushVertex(my::Vec4<real>(gridStartX, 0, (gridCentZ + i) * gridStep));
+	//	rc->pushVertex(my::Vec4<real>(gridEndX, 0, (gridCentZ + i) * gridStep));
+
+	//	rc->pushVertex(my::Vec4<real>((gridCentX + i) * gridStep, 0, gridStartZ));
+	//	rc->pushVertex(my::Vec4<real>((gridCentX + i) * gridStep, 0, gridEndZ));
+	//}
+	//rc->drawLineListZBufferRW(my::Color(0.6f, 0.6f, 0.6f));
+
+	// render the scene
+	m_scene_bsp->draw(rc);
+
+	// update skeleton animation
+	std::string current_anim_name = m_jack_skel->getCurrentAnimationName();
+	if (t3d::vec3Length(m_character.body->getVelocity()) > 1)
 	{
-		rc->pushVertex(my::Vec4<real>(gridStartX, 0, (gridCentZ + i) * gridStep));
-		rc->pushVertex(my::Vec4<real>(gridEndX, 0, (gridCentZ + i) * gridStep));
-
-		rc->pushVertex(my::Vec4<real>((gridCentX + i) * gridStep, 0, gridStartZ));
-		rc->pushVertex(my::Vec4<real>((gridCentX + i) * gridStep, 0, gridEndZ));
+		if(current_anim_name != "clip3" && current_anim_name != "clip4")
+		{
+			m_jack_skel->setCurrentAnimationName("clip3");
+			m_jack_skel->setCurrentAnimationTime(0);
+			m_jack_intersection_offset = 0.5f;
+		}
 	}
-	rc->drawLineListZBufferRW(my::Color(0.6f, 0.6f, 0.6f));
+	else
+	{
+		if(current_anim_name != "clip1" && current_anim_name != "clip2")
+		{
+			m_jack_skel->setCurrentAnimationName("clip1");
+			m_jack_skel->setCurrentAnimationTime(0);
+			m_jack_intersection_offset = 0.5f;
+		}
+	}
 
+	// combine multi skeleton animation to bone node list
+	t3d::BoneIndexList::const_iterator bone_index_iter = m_jack_skel->getRootIndexListBegin();
+	for(; bone_index_iter != m_jack_skel->getRootIndexListEnd(); bone_index_iter++)
+	{
+		const t3d::BoneNodeList & boneNodeList = m_jack_skel->gotoAnimation(
+			m_jack_skel->getCurrentAnimationName(),
+			*bone_index_iter,
+			m_jack_skel->getCurrentAnimationTime() + elapsedTime);
+
+		t3d::incrementBoneNodeList(
+			m_jack_combine_bone_node_list,
+			m_jack_skel->getOrigBoneNodeList(),
+			boneNodeList,
+			*bone_index_iter);
+	}
+
+	// intersection current bone node list to combine bone node list
+	t3d::intersectBoneNodeList(
+		m_jack_bone_node_list,
+		m_jack_bone_node_list,
+		m_jack_combine_bone_node_list,
+		0.5f,
+		0,
+		m_jack_intersection_offset,
+		m_jack_skel->getRootIndexListBegin(),
+		m_jack_skel->getRootIndexListEnd());
+
+	// decrease intersection offset
+	m_jack_intersection_offset = std::max<t3d::real>(0, m_jack_intersection_offset - elapsedTime);
+
+	// calculate character rotation matrix
+	t3d::Mat4<real> jack_mrot = t3d::buildRotationMatrixFromQuatLH(
+		m_character.body->getOrientation() * t3d::buildQuatFromAngleAxis(DEG_TO_RAD(180), my::Vec4<real>::UNIT_Y));
+
+	// gain bone transform list
+	t3d::buildBoneTransformListFromBoneNodeList(
+		m_jack_bone_transform_list,
+		m_jack_bone_node_list,
+		jack_mrot,
+		jack_mrot * t3d::mat3Mov(t3d::vec3Add(m_character.body->getPosition(), my::Vec4<real>(0, -5.0f, 0))),
+		m_jack_skel->getRootIndexListBegin(),
+		m_jack_skel->getRootIndexListEnd());
+
+	// combine final bone transform list
+	t3d::combineVertexNormalBoneTransformList(
+		m_jack_bone_transform_list,
+		m_jack_skel->getOrigBoneInverseTransformList(),
+		m_jack_bone_transform_list);
+
+	m_jack_silhouette_edge_list.clear();
+	for(int i = 0; i < 2; i++)
+	{
+		// combine jack model with bone transform list
+		t3d::bindVertexListNormalListFromBoneTransformList(
+			m_jack_meshs[i]->getVertexList(),
+			m_jack_meshs[i]->getNormalList(),
+			m_jack_bone_assignments[i]->getOriginalVertexList(),
+			m_jack_bone_assignments[i]->getOriginalNormalList(),
+			m_jack_bone_assignments[i]->getBoneAssignmentList(),
+			m_jack_bone_transform_list);
+
+		// draw jack sub mesh
+		m_jack_meshs[i]->draw(rc);
+
+		// calculate Indicator List, use the light at world directly
+		t3d::IndicatorList jack_indicator_list;
+		t3d::buildIndicatorListFromTriangleIndexListByPoint(
+			jack_indicator_list,
+			m_jack_meshs[i]->getVertexList(),
+			m_jack_meshs[i]->getVertexIndexList(),
+			l_pos);
+
+		// calculate silhouette edge list
+		t3d::pushSilhouetteEdgeList(
+			m_jack_silhouette_edge_list,
+			m_jack_connection_edges[i],
+			m_jack_meshs[i]->getVertexList(),
+			jack_indicator_list);
+	}
+
+	// calculate shadow volume
+	m_jack_shadow_volume.clear();
+	t3d::pushUncappedShadowVolumeByPoint(
+		m_jack_shadow_volume,
+		m_jack_silhouette_edge_list,
+		l_pos,
+		300);
+
+	// draw shadow volume
+	rc->clearVertexList();
+	rc->pushVertexList(m_jack_shadow_volume.begin(), m_jack_shadow_volume.end());
+	rc->drawTriangleListShadowVolumeZPass(my::Color(0.39f, 0.39f, 0.39f));
 	return true;
 }
